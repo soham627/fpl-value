c7c3762463693d7cb0011dab4c3909521bf9936c
unknown <malls@uchicago.edu>
2022-01-18 01:44:58 -0500

    'tryin'

--- update_data.py
+++ update_data.py
@@ -1,6 +1,6 @@
 from pandas.io.formats.format import TextAdjustment
 from sqlalchemy.sql.sqltypes import Boolean, Float
-from valuation import db
+import db
 import requests 
 import pandas as pd
 pd.options.mode.chained_assignment = None 
