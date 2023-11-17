3f84081e8f41a3cd81530e54cfa09f6ed75eec92
Soham Mall <malls@uchicago.edu>
2023-11-17 17:01:29 +0000

    updated update_data script to create new data tables for 23/24

--- update_data.py
+++ update_data.py
@@ -181,20 +181,20 @@ teams_2223.to_sql(name='team2223',con=db.engine, index=False, if_exists='replace
 }) """
 
 #COMMENT OUT AFTER FIRST RUN 
-teams_2324 = teams_df[['id','name']]
+"""teams_2324 = teams_df[['id','name']]
 
 teams_2324.to_sql(name='team2324',con=db.engine, index=False, if_exists='replace',dtype={
     'id': Integer,
     'name': Text
-}) 
+}) """
 
 
 con = sqlalchemy.create_engine(uri, encoding='utf8')
 ## removing the foreign key constraints after the tables already exist so that they can be dropped and replaced
 
 ## COMMENT THIS BACK IN AFTER FIRST RUN 
-#con.execute('alter table record drop constraint teamer')
-#con.execute('alter table record drop constraint player_connect')
+con.execute('alter table record drop constraint teamer')
+con.execute('alter table record drop constraint player_connect')
 
 ### saving tables from 21-22 season - COMMENT THIS OUT AFTER FIRST RUN
 
@@ -203,9 +203,8 @@ con = sqlalchemy.create_engine(uri, encoding='utf8')
 #con.execute('create table a_record_2122 AS TABLE record')
 
 ### saving tables from 22-23 season - COMMENT THIS OUT AFTER FIRST RUN
-con.execute('create table a_player_2223 AS TABLE player')
-
-con.execute('create table a_record_2223 AS TABLE record')
+#con.execute('create table a_player_2223 AS TABLE player')
+#con.execute('create table a_record_2223 AS TABLE record')
 
 
 player_overview_df.to_sql(name='player', con=db.engine, index=False, if_exists='replace', dtype={
@@ -282,7 +281,7 @@ con.execute('alter table player add primary key(id)')
 ## Comment out after first run
 #con.execute('alter table team2122 add primary key(id)')
 #con.execute('alter table team2223 add primary key(id)')
-con.execute('alter table team2324 add primary key(id)')
+#con.execute('alter table team2324 add primary key(id)')
 
 
 con.execute('alter table record add primary key(id)')
